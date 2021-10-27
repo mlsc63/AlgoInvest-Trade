3ebbd10027f6f0cf5eac9e66df76399ed535acfe
mlescure <mlescure@mlesucre.fr>
2021-10-27 14:48:58 +0200

    1

--- optimized/main.py
+++ optimized/main.py
@@ -7,14 +7,15 @@ def open_csv():
         line_count = 0
         data = []
 
+
         for row in csv_reader:
             if line_count != 0:
-                if row[1] != '0.0' and '-' not in row[1]:
-                    my_liste = (row[0], float(row[1]), float(row[2]))
+                if row[1] != '0.0' and '-' not in row[1] and row[2] != '0.0':
+                    taux = round((float(row[1]) * float(row[2])), 2) *100
+                    my_liste = (row[0], float(row[1]), taux, float(row[2]))
                     data.append(my_liste)
             line_count += 1
         return data
-
 
 def sacADos_dynamique(capacite, elements):
     matrice = [[0 for x in range(capacite * 100 + 1)] for x in range(len(elements) + 1)]
@@ -22,12 +23,11 @@ def sacADos_dynamique(capacite, elements):
     for i in range(1, len(elements) + 1):
         for w in range(1, capacite * 100 + 1):
             if int(elements[i - 1][1] * 100) <= w:
+
                 matrice[i][w] = round(max(elements[i - 1][2] + matrice[i - 1][w - int(elements[i - 1][1] * 100)],
                                           int(matrice[i - 1][w])), 2)
-
             else:
-                matrice[i][w] = matrice[i - 1][w]
-
+                matrice[i][w] = round(matrice[i - 1][w], 2)
     w = capacite * 100
     n = len(elements)
     elements_selection = []
@@ -43,10 +43,12 @@ def sacADos_dynamique(capacite, elements):
 
 
 ele = open_csv()
-test1, test2 = sacADos_dynamique(500, ele)
+test1, test2 = sacADos_dynamique(500, ele) 
 euro = 0
+benefice = 0
 for i in test2:
-    print('Name: ', i[0], ' Pice: ', i[1], ' Profit ', i[2])
-    euro += i[1]
+    print('Name: ', i[0], ' Pice: ', i[1], ' Profit ', i[3])
+    euro += round(i[1], 2)
+    benefice += i[3]
 print(euro)
-# print('Algo dynamique', sacADos_dynamique(500, ele))
+print(benefice)
